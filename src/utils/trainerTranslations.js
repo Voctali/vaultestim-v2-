f6a7aba885f23b9ea3d5dacc5d180b5ff2f1a801
Vaultestim <vaultestim@gmail.com>
2025-10-31 23:55:40 +0100

    feat: Ajout traduction Attention de Tcheren
    Nouvelle traduction ajoutée :
    • Attention de Tcheren → Cheren's Care (Carte Supporter Unova Gen 5)
    
    Fichier : src/utils/trainerTranslations.js

--- src/utils/trainerTranslations.js
+++ src/utils/trainerTranslations.js
@@ -105,6 +105,7 @@ const TRAINER_TRANSLATIONS = {
   'armand': 'riley', // Aura Guardian - Sinnoh
   'armando, rachid et noa': 'chili & cilan & cress', // Trio Gym Leaders Unova (Gen 5)
   'aromathérapeute': 'aroma lady', // Classe de dresseuse
+  'attention de tcheren': 'cheren\'s care', // Carte Supporter Unova (Gen 5)
   'art secret de jeannine': 'janine\'s secret art', // Fuchsia Gym Leader
   'art secret de janine': 'janine\'s secret art', // Variante orthographe anglaise
   'charme d\'atalante': 'lisia\'s appeal', // Championne de Concours Hoenn - Surging Sparks (SSP)
