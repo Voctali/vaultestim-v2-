e8371d41a183d28e987992ca64a7347702b7cf42
Vaultestim <vaultestim@gmail.com>
2025-11-20 19:55:21 +0100

    chore: force rebuild après reconfig variables RapidAPI

--- src/version.js
+++ src/version.js
@@ -16,7 +16,7 @@
  *   - Authentification Supabase
  */
 
-export const APP_VERSION = '1.9.3'
+export const APP_VERSION = '1.10.0'
 export const APP_NAME = 'VaultEstim'
 export const BUILD_DATE = '2025-11-20'
 
