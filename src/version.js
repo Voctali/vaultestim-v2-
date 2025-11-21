b21201122e0b09b7637a2f2a0a1462b15f380642
Vaultestim <vaultestim@gmail.com>
2025-11-21 23:46:04 +0100

    fix: renommage extension Energy en 'Scarlet & Violet Energies' (v1.11.14)

--- src/version.js
+++ src/version.js
@@ -16,7 +16,7 @@
  *   - Authentification Supabase
  */
 
-export const APP_VERSION = '1.11.13'
+export const APP_VERSION = '1.11.14'
 export const APP_NAME = 'VaultEstim'
 export const BUILD_DATE = '2025-11-21'
 
